993e0114c04200419f6eb002f9fdb07d9f2f064e
Salaga2005RUS <armeiski@frtk.ru>
2023-09-04 21:30:51 +0300

    commit commit before sending to Sanya_2

--- header.h
+++ header.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+
 void strcpy_my(char *destination, char *source);
-void printf_str(char *string);
+void puts_my(char *string);
 void strncpy_my(char *destination, char *source, int n);
 void strcat_my(char *destination, char *append);
 int strchr_my(char *string, char ch);
